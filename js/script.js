50a026e53bef4050bcf3ebcf2d4e6f1757806851
Oleg <oleg.shtykanov@gmail.com>
2020-08-22 20:32:10 +0300

    22.08.2020

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const btn_1 = document.querySelector('.button_1')
+const btn_2 = document.querySelector('.button_2')
+const btn_3 = document.querySelector('.button_3')
+
+btn_1.onclick = function() {
+    alert('Это кнопка была для чего-то нажата');
+    console.log('???')
+};
+
+btn_2.onclick = function() {
+    alert('Это кнопка была для чего-то');
+    console.log('???')
+};
+
+btn_3.onclick = function() {
+    alert('Это кнопка была');
+    console.log('???')
+};
+
